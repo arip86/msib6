a17674b93b32a2ad96c07517170e452022ec2c21
arip86 <akhmadarip86@gmail.com>
2024-06-11 12:22:55 +0700

    payment gateway

--- resources/views/front/shop_cart.blade.php
+++ resources/views/front/shop_cart.blade.php
@@ -1,6 +1,8 @@
 @extends('front.layout.app')
 @section('content')
-
+<script type="text/javascript"
+      src="https://app.sandbox.midtrans.com/snap/snap.js"
+      data-client-key="{{config('midtrans.client_key')}}"></script>
 
 <section class="h-100" style="background-color: #eee;">
   <div class="container h-100 py-5">
@@ -17,40 +19,52 @@
 @if(session('cart'))
 @foreach(session('cart') as $id => $details)
 @php $total += $details['harga_jual'] * $details['quantity'] @endphp
-        <div class="card rounded-3 mb-4">
+        <div class="card rounded-3 mb-4" data-id="{{ $id }}">
           <div class="card-body p-4">
             <div class="row d-flex justify-content-between align-items-center">
               <div class="col-md-2 col-lg-2 col-xl-2">
+              @empty($details['foto'])                
                 <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
+                src="{{url('admin/image/nophoto.jpg')}}"
                   class="img-fluid rounded-3" alt="Cotton T-shirt">
+              @else
+                <img
+               src="{{url('admin/image')}}/{{$details['foto']}}"
+                  class="img-fluid rounded-3" alt="Cotton T-shirt">
+              @endempty
               </div>
               <div class="col-md-3 col-lg-3 col-xl-3">
                 <p class="lead fw-normal mb-2">{{$details['nama']}}</p>
-                <p><span class="text-muted">Size: </span>M <span class="text-muted">Color: </span>Grey</p>
+                <span>Rp.{{number_format($details['harga_jual'],0,',','.')}}</span>
+               
               </div>
               <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
                 <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
                   onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                  <i class="fas fa-minus"></i>
+                  <i class="fa fa-minus"></i>
                 </button>
 
-                <input id="form1" min="0" name="quantity" value="2" type="number"
-                  class="form-control form-control-sm" />
+                <input value="{{$details['quantity']}}" type="number"
+                  class="form-control quantity update-cart" />
 
                 <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
                   onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                  <i class="fas fa-plus"></i>
+                  <i class="fa fa-plus"></i>
                 </button>
               </div>
               <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                <h5 class="mb-0">$499.00</h5>
+                <h5 class="mb-0">Rp. {{$details['harga_jual'] * $details['quantity']}}</h5>
               </div>
               <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                <a href="#!" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>
+                <button  class="btn btn-danger btn-sm remove-from-cart"><i class="fa fa-trash fa-lg"></i></button>
               </div>
+              
             </div>
+            <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+            
+              </div>
           </div>
+          
         </div>
 
        
@@ -58,9 +72,19 @@
         @endif
 
         <div class="card">
-          <div class="card-body">
-            <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-warning btn-block btn-lg">Proceed to Pay</button>
+        <div class="card-body p-4">
+        <div class="row d-flex justify-content-between align-items-center">
+        <div class="col-md-6 col-lg-6 col-xl-6">
+            <button  type="button"
+            class="btn btn-warning btn-block btn-lg" id="pay-button">Proceed to Pay</button>
+        </div>
+          <div class="col-md-3 col-lg-2 col-xl-4 offset-lg-1">
+                <h5 class="mb-0">Total Rp. {{number_format($total,0,',','.')}}</h5>
+              </div>
+              
+      </div>
           </div>
+
         </div>
 
       </div>
@@ -69,4 +93,73 @@
 </section>
 
 
+
+<script type="text/javascript">
+      // For example trigger on button clicked, or any time you need
+      var payButton = document.getElementById('pay-button');
+      payButton.addEventListener('click', function () {
+        // Trigger snap popup. @TODO: Replace TRANSACTION_TOKEN_HERE with your transaction token
+        window.snap.pay('{{$snapToken}}', {
+          onSuccess: function(result){
+            /* You may add your own implementation here */
+            alert("payment success!"); console.log(result);
+          },
+          onPending: function(result){
+            /* You may add your own implementation here */
+            alert("wating your payment!"); console.log(result);
+          },
+          onError: function(result){
+            /* You may add your own implementation here */
+            alert("payment failed!"); console.log(result);
+          },
+          onClose: function(){
+            /* You may add your own implementation here */
+            alert('you closed the popup without finishing the payment');
+          }
+        })
+      });
+    </script>
+    @endsection
+@section('scripts')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script type="text/javascript">
+ 
+    $(".update-cart").change(function (e) {
+        e.preventDefault();
+ 
+        var ele = $(this);
+        var id = ele.closest('.card').data('id');
+ 
+        $.ajax({
+            url: '{{ route('update.cart') }}',
+            method: "patch",
+            data: {
+                _token: '{{ csrf_token() }}',
+                id: id,
+                quantity: ele.val()
+            },
+            success: function (response) {
+               window.location.reload();
+            }
+        });
+    });
+    $(".remove-from-cart").click(function (e) {
+        e.preventDefault();
+        var ele = $(this);
+        if(confirm("Are you sure want to remove?")) {
+            $.ajax({
+                url: '{{ route('remove.from.cart') }}',
+                method: "DELETE",
+                data: {
+                    _token: '{{ csrf_token() }}',
+                    id: ele.parents("tr").attr("data-id")
+                },
+                success: function (response) {
+                    window.location.reload();
+                }
+            });
+        }
+    });
+ 
+</script>
 @endsection
